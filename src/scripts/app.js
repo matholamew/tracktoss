4ea6a20469441d33d4ceba82a840975da920ed11
matholamew <matthew+github@manderson.me>
2025-03-28 10:51:17 -0400

    import for the qrcode function adjustments

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,9 @@
 import { supabase } from './supabase-client.js'
 import { musicService } from './music-services.js'
+import qrcode from 'qrcode-generator'
+
+// Global variables
+let currentPlaylistId = null
 
 // DOM Elements
 const startPlaylistBtn = document.getElementById('startPlaylistBtn')
@@ -25,80 +29,8 @@ closeScannerBtn.addEventListener('click', handleCloseScanner)
 closeSearchModalBtn.addEventListener('click', handleCloseSearchModal)
 searchInput.addEventListener('input', debounce(handleSearch, 300))
 
-// Auth Event Listeners
-document.getElementById('spotifyLoginBtn').addEventListener('click', () => musicService.authenticateSpotify())
-document.getElementById('youtubeLoginBtn').addEventListener('click', () => musicService.authenticateYouTube())
-
-// Check for auth callbacks
-window.addEventListener('load', () => {
-  const hash = window.location.hash
-  if (hash) {
-    if (window.location.pathname.includes('/auth/spotify')) {
-      if (musicService.handleSpotifyCallback(hash)) {
-        window.location.hash = ''
-        updateAuthUI()
-      }
-    } else if (window.location.pathname.includes('/auth/youtube')) {
-      if (musicService.handleYouTubeCallback(hash)) {
-        window.location.hash = ''
-        updateAuthUI()
-      }
-    }
-  }
-})
-
-// Update UI based on auth status
-function updateAuthUI() {
-  const spotifyBtn = document.getElementById('spotifyLoginBtn')
-  const youtubeBtn = document.getElementById('youtubeLoginBtn')
-
-  if (musicService.isSpotifyAuthenticated()) {
-    spotifyBtn.innerHTML = `
-      <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
-      </svg>
-      Connected to Spotify
-    `
-    spotifyBtn.classList.add('bg-[#1DB954]')
-    spotifyBtn.classList.remove('bg-gray-400')
-  } else {
-    spotifyBtn.innerHTML = `
-      <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
-      </svg>
-      Connect Spotify
-    `
-    spotifyBtn.classList.remove('bg-[#1DB954]')
-    spotifyBtn.classList.add('bg-gray-400')
-  }
-
-  if (musicService.isYouTubeAuthenticated()) {
-    youtubeBtn.innerHTML = `
-      <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-      </svg>
-      Connected to YouTube
-    `
-    youtubeBtn.classList.add('bg-[#FF0000]')
-    youtubeBtn.classList.remove('bg-gray-400')
-  } else {
-    youtubeBtn.innerHTML = `
-      <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-      </svg>
-      Connect YouTube
-    `
-    youtubeBtn.classList.remove('bg-[#FF0000]')
-    youtubeBtn.classList.add('bg-gray-400')
-  }
-}
-
-// Initialize auth UI
-updateAuthUI()
-
 let scanner = null
 let scannerStream = null
-let currentPlaylistId = null
 
 // Debounce function for search input
 function debounce(func, wait) {
