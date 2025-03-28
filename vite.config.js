3007d62ac063f2f9e2c7b61850f0514e011fdba4
matholamew <matthew+github@manderson.me>
2025-03-28 10:58:32 -0400

    Added instascan to the external dependencies

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
       },
-      external: ['qrcode-generator']
+      external: ['qrcode-generator', 'instascan']
     },
     copyPublicDir: true
   },
